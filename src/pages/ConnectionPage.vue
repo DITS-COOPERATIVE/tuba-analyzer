15795b2c90fad5f14fd13f9bbf00150248bb1473
agcodex01 <agcodex01@gmail.com>
2022-10-26 20:35:08 +0800

    initialize project

---
+++ src/pages/ConnectionPage.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div style="width: 300px">
+      <div class="text-center q-mb-lg">
+        <h5 class="q-mb-sm">
+          Bluetooth
+          <q-icon color="primary" name="bluetooth" />
+        </h5>
+        <q-toggle v-model="value" color="green" label="Turn On" left-label/>
+        <q-btn   v-if="value" class="block full-width" label="Scan Devices"/>
+      </div>
+
+      <q-list bordered separator>
+        <q-item clickable v-ripple>
+          <q-item-section>Single line item</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section>
+            <q-item-label>Item with caption</q-item-label>
+            <q-item-label caption>Caption</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section>
+            <q-item-label overline>OVERLINE</q-item-label>
+            <q-item-label>Item with caption</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+  </q-page>
+</template>
+
+<script type="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'ConnectionPage',
+  setup () {
+    return {
+      value: ref(true)
+    }
+  }
+})
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div class="text-center">
+      <h5>Tuba Analyzer App</h5>
+      <q-img src="~/assets/logo.jpg"></q-img>
+      <q-btn :to="{ name: 'connection' }" style="width: 200px;" color="primary" size="lg" rounded outline
+        class="q-mt-xl" label="Start"></q-btn>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
